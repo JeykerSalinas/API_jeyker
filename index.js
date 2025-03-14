5ab45ca78289c55b71a59100eafd431dd2c0b22a
Jeyker Salinas <jeyker.salinas13@gmail.com>
2025-03-14 19:02:23 +0100

    Post method

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const PORT = process.env.PORT || 3001;
 const chance = new Chance();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // Para manejar JSON en las peticiones
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<think>Bot thinking part</think>" + chance.paragraph({ sentences: 2 }));
+app.post("/", (req, res) => {
+  res.send({
+    text:
+      "<think>Bot thinking part</think>" +
+      chance.paragraph({ sentences: chance.integer({ min: 1, max: 3 }) }),
+    type: "chat_message",
+  });
 });
 
-app.listen(PORT, () => console.log(`Ejecutando en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Working on ${PORT}`));
