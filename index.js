d18ea46ff1aacc6aa4cdd41ce80e31f1cfe25e87
Jeyker Salinas <jeyker.salinas13@gmail.com>
2025-03-14 12:40:16 +0100

    Fix endpoint

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
-
+import { Chance } from "chance";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const chance = new Chance();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Para manejar JSON en las peticiones
 
 app.get("/", (req, res) => {
-  res.send("everto marico");
+  res.send("<think>Bot thinking part</think>" + chance.paragraph({ sentences: 2 }));
 });
 
 app.listen(PORT, () => console.log(`Ejecutando en el puerto ${PORT}`));
